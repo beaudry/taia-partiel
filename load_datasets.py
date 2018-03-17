feb0d9ef44ef8c14603d6164ce16be049c9ab38e
Charles-André  Beaudry <charles-andre.beaudry.1@ulaval.ca>
2018-03-17 12:03:29 -0400

    Started congress

--- load_datasets.py
+++ load_datasets.py
@@ -95,9 +95,18 @@ def load_congressional_dataset(train_ratio):
     # Le fichier du dataset est dans le dossier datasets en attaché 
     f = open('datasets/house-votes-84.data', 'r')
 
-	
     # TODO : le code ici pour lire le dataset
+    dataset = []
     
+    for line in f:
+        data = line.replace('\n', '').split(',')
+        if data != ['']:
+            for i in range(len(data)):
+                data[i] = conversion_labels[data[i]]
+            dataset.append(data)
+
+    random.shuffle(dataset)
+    print(dataset)
 	
 	# La fonction doit retourner 4 structures de données de type Numpy.
     return (train, train_labels, test, test_labels)
@@ -138,4 +147,4 @@ def load_monks_dataset(numero_dataset):
 
     # La fonction doit retourner 4 matrices (ou vecteurs) de type Numpy. 
     return (train, train_labels, test, test_labels)
-load_iris_dataset(0.5)
+load_congressional_dataset(0.5)
