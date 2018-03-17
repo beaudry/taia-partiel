a5ce61d15e7dd96afd2bf3cf56badc1ec8587bce
Charles-André  Beaudry <charles-andre.beaudry.1@ulaval.ca>
2018-03-17 17:25:32 -0400

    Predict and train

--- Knn.py
+++ Knn.py
@@ -1,5 +1,7 @@
 # coding=UTF-8
 import numpy as np
+import math
+import heapq
 
 class Knn: 
 
@@ -9,7 +11,21 @@ class Knn:
 		Vous pouvez passer d'autre paramètres au besoin,
 		c'est à vous d'utiliser vos propres notations
 		"""
+		self.neighbors_number = 3
+		self.data = np.array([])
+		self.labels = np.array([])
+
+	def instance_distance(self, instance1, instance2):
+		total_distance = 0
+
+		for i in range(len(instance1)):
+			total_distance += self.attr_distance(instance1[i], instance2[i])
+
+		return math.sqrt(total_distance)
 		
+	def attr_distance(self, attr1, attr2):
+		return (attr1 - attr2) ** 2
+
 		
 	def train(self, train, train_labels):
 		"""
@@ -23,6 +39,7 @@ class Knn:
 		vous pouvez rajouter d'autres arguments, il suffit juste de
 		les expliquer en commentaire
 		
+
 		
 		
 		------------
@@ -38,6 +55,44 @@ class Knn:
 		Bien entendu ces tests doivent etre faits sur les données d'entrainement
 		nous allons faire d'autres tests sur les données de test dans la méthode test()
 		"""
+		self.data = train[:]
+		self.labels = train_labels[:]
+		accuracy = 0
+		predicted_list = np.array([self.predict(self.data[i], self.labels[i]) for i in range(len(self.data))])
+
+		confusion_object = {}
+
+		for i in range(len(self.data)):
+			predicted_label = predicted_list[i]
+			real_label = self.labels[i]
+
+			if predicted_label not in confusion_object:
+				confusion_object[predicted_label] = {}
+			
+			if predicted_label is real_label:
+				accuracy += 1
+			
+			if real_label not in confusion_object[predicted_label]:
+				confusion_object[predicted_label][real_label] = 0
+
+			confusion_object[predicted_label][real_label] += 1
+		
+		for predicted_label in np.sort(predicted_list):
+			if predicted_label not in confusion_object:
+				confusion_object[predicted_label] = {}
+
+			for real_label in self.labels:
+				if real_label not in confusion_object[predicted_label]:
+					confusion_object[predicted_label][real_label] = 0
+				
+		#confusion matrix	
+		print(confusion_object[predicted_list[0]].keys())
+		for predicted_label in confusion_object.keys():
+			print(predicted_label, confusion_object[predicted_label].values())
+		
+		
+		print(accuracy / len(self.data))
+		
 
 	def predict(self, exemple, label):
 		"""
@@ -48,6 +103,19 @@ class Knn:
 		alors l'exemple est bien classifié, si non c'est une missclassification
 
 		"""
+		
+		nearest_neighbors = []
+		for i in range(self.neighbors_number):
+			nearest_neighbors.append((-np.inf, None))
+		
+		for i in range(len(self.data)):
+			distance = -self.instance_distance(self.data[i], exemple)
+			if nearest_neighbors[0][0] < distance:
+				heapq.heapreplace(nearest_neighbors, (distance, self.labels[i]))
+		
+
+		return int(round(np.average([neighbor[1] for neighbor in nearest_neighbors])))
+
 
 	def test(self, test, test_labels):
 		"""
